76d40fa0703407a938cbf1d8302c3b6464cabb31
Nushikito <probnijnikita@gmail.com>
2024-11-21 23:42:18 +0500

    Создана 3-ая функция

--- main.cpp
+++ main.cpp
@@ -17,16 +17,25 @@ void ReadingFile(vector<string> &poem)
         cout << x << endl;
     }
 }
-void RecordFile()
+void RecordFile(const string& name, vector<string> &poem)
 {
-
+    ofstream output_file(name);
+    for (const auto& line : poem) {
+        output_file << line << endl;
+    }
+    output_file.close();
 }
 
 int main()
 {
-
+    /* vector<string> poem = {
+        "first string",
+        "second string",
+        "third string"
+    }; */
     OpenFile();
-    ReadingFile();
-    RecordFile();
+    ReadingFile(poem);
+    string name = "output.txt";
+    RecordFile(name, poem);
 
 }
